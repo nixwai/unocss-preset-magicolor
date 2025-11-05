f7015a1e670fd958fa9e1fc3a62a2384d51352f9
nixiaoyu <1149062971@qq.com>
2025-11-05 10:59:22 +0800

    feat: update function todo

--- packages/presets/src/helper.ts
+++ packages/presets/src/helper.ts
@@ -1,9 +1,3 @@
-import type { CSSValue } from 'unocss';
-
-export function updateDomStyle(style: CSSValue, dom?: HTMLElement) {
-  if (dom) {
-    Object.entries(style).forEach(([key, value]) => {
-      dom.style.setProperty(key, value);
-    });
-  }
+export function updateMagicColor() {
+  // TODO
 }
