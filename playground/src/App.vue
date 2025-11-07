2badd3a96bc00f4c19104b35e725c0150a7d7e7c
nixiaoyu <1149062971@qq.com>
2025-11-07 17:36:07 +0800

    feat: 更新示例

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import { updateMagicColor } from '../../packages/presets/src/helper';
 
-const Div = ref<HTMLElement>();
-
 function toggleColor() {
-  updateMagicColor({ name: 'btn', color: 'rgb(79 123 333)', dom: Div.value });
+  updateMagicColor({ name: 'primary', color: 'rgb(79 123 333)', dom: document.documentElement });
 }
 </script>
 
 <template>
   <div class="w-100vw h-100vh flex items-center justify-center flex-col">
-    <div ref="Div" class="mc-btn_oklch(0.1_0.2_122)-100">
+    <div class="mc-btn_oklch(0.1_0.2_122)-100">
       <button class="px-8 py-4 hover:bg-mc-btn-550 bg-mc-btn-640">
         <span class="c-mc-btn-200">
           Hello World!
