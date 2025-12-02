0a1dcd900ac1d91cef1afc51985c70f6da8cb9a0
nixiaoyu <1149062971@qq.com>
2025-12-02 20:44:20 +0800

    refactor: optimize build

--- packages/build-system/build-presets/paths.ts
+++ packages/build-system/build-presets/paths.ts
@@ -1,8 +1,3 @@
-import { resolve } from 'node:path';
-import { pkgRoot } from '../build-paths';
+import { createModulePaths } from '../build-paths';
 
-/** 预设库代码根目录 */
-export const presetRoot = resolve(pkgRoot, 'presets');
-
-/** 预设库打包目录 */
-export const presetOutput = resolve(presetRoot, 'dist');
+export const [presetRoot, presetOutput] = createModulePaths('presets');
